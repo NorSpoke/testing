63e384b36ec0ec78965076c627eac8249584de2e
NorSpoke <147317131+NorSpoke@users.noreply.github.com>
2025-11-29 04:32:10 +0800

    Update and rename main.js to homePage/home.js

---
+++ homePage/home.js
@@ -0,0 +1,7 @@
+const level1Button = document.getElementById("level1-button")
+const level2Button = document.getElementById("level2-button")
+const level3Button = document.getElementById("level3-button")
+
+level1Button.addEventListener("click", function() {
+    window.location.href = "../level1/game.html"
+})
